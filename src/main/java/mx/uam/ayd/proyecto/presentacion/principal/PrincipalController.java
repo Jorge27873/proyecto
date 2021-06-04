bc7ed8c9dd6dcbac8af40ff05d116f4c7b7444b1
Juan Carlos Quiroz Aguilar <jcqa24@gmail.com>
2021-06-04 10:22:42 -0500

    login sin contraseña

--- src/main/java/mx/uam/ayd/proyecto/presentacion/principal/PrincipalController.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/principal/PrincipalController.java
@@ -3,13 +3,18 @@ package mx.uam.ayd.proyecto.presentacion.principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import mx.uam.ayd.proyecto.negocio.ServicioUsuario;
 /**
  * 
  * Controlador principal
  * 
  *
  */
+@Slf4j
 @Controller
 public class PrincipalController {
 	
@@ -20,12 +25,26 @@ public class PrincipalController {
 	 * @return
 	 */
 	@RequestMapping(value = "/")
-    public String vistaPrincipal() {
+    public String vistaPrincipal(Model model) {
         
 			// Redirige a la vista principal
-    		//return "vistaPrincipal/Principal";
-    		return "vistaUsuarios/vistaUsuarios";
+    		return "vistaPrincipal/Principal";
+    		
     }
+	
+	
+	@RequestMapping(value = "/vistaUsuarios" , method = RequestMethod.GET)
+    public String vistaUsuarios(
+    		@RequestParam(value="id", required=true) long id,
+    		Model model) {
+		try {
+			model.addAttribute("id",id);
+		
+	} catch(Exception ex) {
+		log.info(ex.getMessage());
+		
+	}
+		return "vistaUsuarios/vistaUsuarios";
 
-
+	}
 }
